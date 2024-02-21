1530b6f0a3e12e2543b1f285e24fd7160e27873c
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-02-21 15:39:44 +0530

    Update LoginServlet.java

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -1,3 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     private static final String JDBC_URL = "jdbc:mysql://192.168.138.114:3306/myDB";
